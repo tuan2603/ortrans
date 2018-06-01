a756b8441bc0808b993300e5a44ed6347be7bf4b
Trần minh tuấn <tuan260387@gmail.com>
2018-06-02 03:47:09 +0700

    ddd

--- controllers/captchaController.js
+++ controllers/captchaController.js
@@ -2,34 +2,33 @@
 
 const request_promise = require('request-promise');
 
-exports.recaptcha_verify = function(req, res) {
+exports.recaptcha_verify = function (req, res) {
 
-        var options = {
-            method: 'POST',
-            uri: 'https://www.google.com/recaptcha/api/siteverify',
-            form: {
-                secret: '6LfPfVwUAAAAAFs896v-B4rzTILIYqhtSy_wjfbb',
-                response: req.body.token
-            },
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-            }
-        };
+    var options = {
+        method: 'POST',
+        uri: 'https://www.google.com/recaptcha/api/siteverify',
+        form: {
+            secret: '6LfPfVwUAAAAAFs896v-B4rzTILIYqhtSy_wjfbb',
+            response: req.body.token
+        },
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    };
 
-        request_promise(options)
-            .then(function (body) {
-                // POST succeeded...
-            return  res.json({
-                "response": body
-            });
-
+    request_promise(options)
+        .then(function (body) {
+            let obj = JSON.parse(body);
+            return res.json({
+                "response": obj.success
             })
-            .catch(function (err) {
-                if (err) return  res.json({
-                    "response": false,
-                    "value":err
-                });
+        })
+        .catch(function (err) {
+            if (err) return res.json({
+                "response": false,
+                "value": err
             });
+        });
 
 };
 
