84f998895a2af1b543e891f203d363afd5223417
Trần minh tuấn <tuan260387@gmail.com>
2018-06-04 11:43:51 +0700

    AAAA

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ routes(app);
 app.use(express.static(path.join(__dirname, 'build')));
 
 // app.get('/', function (req, res) {
-// //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// // });
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
